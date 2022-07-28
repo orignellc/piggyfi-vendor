29ae563a9fea92bc99957fefbb50cfe473986d1c
Oluwafemi Alofe <oluwafemialofe@yahoo.com>
2022-07-29 02:07:07 +0400

    WIP: Proxy, GP2P contracts

---
+++ src/smart-contracts/scripts/deploy.js
@@ -0,0 +1,21 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const GlobalP2P = await hre.ethers.getContractFactory("GlobalP2P");
+  const gp2p = await GlobalP2P.deploy();
+
+  await gp2p.deployed();
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
